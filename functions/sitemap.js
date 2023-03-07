124d651813b525911a904929d599fca22102cb71
liamkopke <liamkopke@gmail.com>
2023-03-07 11:19:26 -0500

    not .text()

--- functions/sitemap.js
+++ functions/sitemap.js
@@ -2,7 +2,7 @@ exports.handler = async (event, condition) => {
     let response
     try{
         await fetch("https://www2.zoetis.ca/sitemap.xml")
-        .then((res) => response = res.text())
+        .then((res) => response = res)
     }
     catch (err){
         return{
