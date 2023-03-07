43b239fba59ddee98486d3e4934d5488cca76b87
liamkopke <liamkopke@gmail.com>
2023-03-07 12:04:33 -0500

    axios for just function

--- functions/sitemap.js
+++ functions/sitemap.js
@@ -1,15 +1,17 @@
 const axios = require('axios');
 
-exports.handler = async (event, condition) => {
-    const url = "https://www2.zoetis.ca/sitemap.xml";
-    axios.get(url)
-        .then((response) => {
-            console.log(response.json());
-            console.log(response.data);
-            console.log(response);
-            return {
-                statusCode: 200,
-                body: response
+exports.handler = function(event, condition, callback) {
+    axios.get("https://www2.zoetis.ca/sitemap.xml")
+    .then(response => {
+        callback(null, {
+            statusCode: 200,
+            body: response.data,
+            headers:{
+                'Content-Type': 'application/xml'
             }
-        })        
+        });
+    })
+    .catch(error => {
+        callback(error);
+    })
 };
