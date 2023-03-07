e1537c6986b78911b5a26a75d3453e0c0fa1468f
liamkopke <liamkopke@gmail.com>
2023-03-07 12:00:14 -0500

    alot of logs

--- functions/sitemap.js
+++ functions/sitemap.js
@@ -2,20 +2,14 @@ const axios = require('axios');
 
 exports.handler = async (event, condition) => {
     const url = "https://www2.zoetis.ca/sitemap.xml";
-    fetch(url)
+    axios.get(url)
         .then((response) => {
             console.log(response.json());
+            console.log(response.data);
             console.log(response);
             return {
                 statusCode: 200,
-                body: response.json()
+                body: response
             }
-        })
-        .catch((err) => {
-            console.log(err);
-            return{
-                statusCode: err.statusCode || 500,
-                body: JSON.stringify({error: err.message})
-            }
-        })
+        })        
 };
