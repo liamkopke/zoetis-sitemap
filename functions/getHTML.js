9213864c6e29480ae2f28e18b3c4a4a01f66ddd2
liamkopke <liamkopke@gmail.com>
2023-03-14 10:17:32 -0400

    doc.createleemtn

--- functions/getHTML.js
+++ functions/getHTML.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
-const HTMLParser = require('node-html-parser');
 
 exports.handler = (event, context, callback) => {    
     console.log(event.queryStringParameters.link);
     axios.get("https://www2.zoetis.ca" + event.queryStringParameters.link)
     .then(response => {
         if(response.status === 200){
-            const data = HTMLParser.parse(response.data);
-            console.log(data)
-            console.log(data.querySelectorAll('a'))
+            const data = document.createElement('div')
+            data.innerHTML = response.data;
+            console.log(data);
+            console.log(data.links);
             var arr = [], l = data.links;
             for(var i=0; i<l.length; i++) {
                 if(arr.indexOf(l[i].href) === -1){  
