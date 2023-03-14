85b8c571a239e11398fc81be061743d7cdb67d2a
liamkopke <liamkopke@gmail.com>
2023-03-14 11:36:29 -0400

    logs?

--- functions/getHTML.js
+++ functions/getHTML.js
@@ -4,10 +4,11 @@ const puppeteer = require('puppeteer');
 exports.handler = async (event, context, callback) => { 
     try{
         const url = "https://www2.zoetis.ca" + event.queryStringParameters.link;
+        console.log(url);
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.goto(url, {
-            waitUntil: 'networkidle2'
+            waitUntil: 'networkidle0'
         });  
         var hrefs = await page.$$eval('a', as => as.map(a => a.href));
         console.log(hrefs)
