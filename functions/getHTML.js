6908bb78b70714a83fa7c00093dba2b4cbc3ce53
liamkopke <liamkopke@gmail.com>
2023-03-14 09:48:26 -0400

    added rundangerously

--- functions/getHTML.js
+++ functions/getHTML.js
@@ -7,13 +7,11 @@ exports.handler = (event, context, callback) => {
     axios.get("https://www2.zoetis.ca" + event.queryStringParameters.link)
     .then(response => {
         if(response.status === 200){
-            const html = new JSDOM(response.data)
-            console.log(html.window.document)
-            console.log(html.window.document.links)
-            var arr = [], l = html.window.document.links;
-            for(var i=0; i<l.length; i++) {
-                if(arr.indexOf(l[i].href) === -1){  
-                    arr.push(l[i].href);
+            const html = (new JSDOM(response.data, { runScripts: "dangerously" })).window.document.links
+            var arr = [];
+            for(var i=0; i<html.length; i++) {
+                if(arr.indexOf(html[i].href) === -1){  
+                    arr.push(html[i].href);
                 }
             }
             callback(null, {
