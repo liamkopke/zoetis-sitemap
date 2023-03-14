7c01418321f67e1e998cbdb85a4025d604f6ffa6
liamkopke <liamkopke@gmail.com>
2023-03-14 09:50:29 -0400

    forgot to put logs back

--- functions/getHTML.js
+++ functions/getHTML.js
@@ -7,11 +7,13 @@ exports.handler = (event, context, callback) => {
     axios.get("https://www2.zoetis.ca" + event.queryStringParameters.link)
     .then(response => {
         if(response.status === 200){
-            const html = (new JSDOM(response.data, { runScripts: "dangerously" })).window.document.links
-            var arr = [];
-            for(var i=0; i<html.length; i++) {
-                if(arr.indexOf(html[i].href) === -1){  
-                    arr.push(html[i].href);
+            const { document } = (new JSDOM(response.data, { runScripts: "dangerously" })).window
+            console.log(document)
+            console.log(document.links)
+            var arr = [], l = document.links;
+            for(var i=0; i<l.length; i++) {
+                if(arr.indexOf(l[i].href) === -1){  
+                    arr.push(l[i].href);
                 }
             }
             callback(null, {
