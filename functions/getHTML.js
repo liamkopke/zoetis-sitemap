e977f8a68b2cb4b0adfdbd18cd6da6e6ee2ad8f6
liamkopke <liamkopke@gmail.com>
2023-03-14 11:15:21 -0400

    puppeteer

--- functions/getHTML.js
+++ functions/getHTML.js
@@ -1,31 +1,29 @@
 const axios = require('axios');
-const jsdom = require('jsdom');
+const puppeteer = require('puppeteer');
 
-exports.handler = (event, context, callback) => {    
-    console.log(event.queryStringParameters.link);
-    axios.get("https://www2.zoetis.ca" + event.queryStringParameters.link)
-    .then(response => {
-        if(response.status === 200){
-            console.log(response.data)
-            const dom = new jsdom.JSDOM(response.data);
-            var arr = []
-            console.log(dom.window.document.querySelectorAll('a'));
-            dom.window.document.querySelectorAll('a').forEach(a => {
-                if(arr.indexOf(a.textContent) === -1){  
-                    arr.push(a.textContent);
-                }
-            });
-            callback(null, {
-                statusCode: 200,
-                body: arr,
-                headers:{
-                    'Content-Type': 'application/json'
-                }
-            })
-        }        
-    })
-    .catch(error => {
-            callback(error)
-        }
-    )
+exports.handler = async (event, context, callback) => { 
+    try{
+        const url = "https://www2.zoetis.ca" + event.queryStringParameters.link;
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url, {
+            waitUntil: 'networkidle2'
+        });  
+        var hrefs = await page.$$eval('a', as => as.map(a => a.href));
+        console.log(hrefs)
+        await browser.close();
+        callback(null, {
+            statusCode: 200,
+            body: hrefs,
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+    catch(error){
+        callback(null, {
+            statusCode: 500,
+            body: error
+        });
+    }    
 };
