67045e15f9222754401b0fccde340530d2c45f92
liamkopke <liamkopke@gmail.com>
2023-03-14 09:20:56 -0400

    added cheerio

--- functions/getHTML.js
+++ functions/getHTML.js
@@ -1,24 +1,28 @@
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 exports.handler = (event, context, callback) => {    
     console.log(event.queryStringParameters.link);
     axios.get("https://www2.zoetis.ca" + event.queryStringParameters.link, {responseType: 'document'})
-    .then(html => {
-        console.log(html);
-        console.log(html.links);
-        var arr = [], l = html.links;
-        for(var i=0; i<l.length; i++) {
-            if(arr.indexOf(l[i].href) === -1){  
-                arr.push(l[i].href);
+    .then(response => {
+        if(response.status === 200){
+            const html = cheerio.load(response.data);
+            console.log(html);
+            console.log(html.links);
+            var arr = [], l = html.links;
+            for(var i=0; i<l.length; i++) {
+                if(arr.indexOf(l[i].href) === -1){  
+                    arr.push(l[i].href);
+                }
             }
-        }
-        callback(null, {
-            statusCode: 200,
-            body: arr,
-            headers:{
-                'Content-Type': 'application/json'
-            }
-        })
+            callback(null, {
+                statusCode: 200,
+                body: arr,
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            })
+        }        
     })
     .catch(error => {
             callback(error)
