d5a3dcfaa8be97502fc6eb391c9c29e932756fa5
liamkopke <liamkopke@gmail.com>
2023-03-21 15:31:23 -0400

    test

--- functions/sitemap.js
+++ functions/sitemap.js
@@ -1,17 +1,31 @@
-const axios = require('axios');
+const axios = require("axios");
+const fs = require("fs");
 
-exports.handler = function(event, condition, callback) {
-    axios.get("https://www2.zoetis.ca/sitemap.xml")
-    .then(response => {
-        callback(null, {
-            statusCode: 200,
-            body: response.data,
-            headers:{
-                'Content-Type': 'application/xml'
-            }
-        });
-    })
-    .catch(error => {
-        callback(error);
-    })
+exports.handler = function (event, condition, callback) {
+	try {
+		const data = JSON.parse(fs.readFileSync("src/sitemap.json").toString());
+		callback(null, {
+			statusCode: 200,
+			body: data,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+	} catch (err) {
+		callback(err);
+	}
+	// axios
+	// 	.get("https://www2.zoetis.ca/sitemap.xml")
+	// 	.then((response) => {
+	// 		callback(null, {
+	// 			statusCode: 200,
+	// 			body: response.data,
+	// 			headers: {
+	// 				"Content-Type": "application/xml",
+	// 			},
+	// 		});
+	// 	})
+	// 	.catch((error) => {
+	// 		callback(error);
+	// 	});
 };
