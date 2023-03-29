fc9d62bc99a2d7bcdc0a96e3be6f3e807421565f
liamkopke <liamkopke@gmail.com>
2023-03-29 14:38:54 -0400

    back to what worked

--- functions/sitemap.js
+++ functions/sitemap.js
@@ -1,31 +1,19 @@
 const axios = require("axios");
 const obj = {};
 
-exports.handler = async function (event, condition, callback) {
+exports.handler = function (event, condition, callback) {
 	axios
 		.get("https://www2.zoetis.ca/sitemap.xml")
 		.then((response) => {
-			console.log(response.data);
-			obj["/www2.zoetis.ca"] = response.data;
-			console.log(obj);
-			axios
-				.get("https://www.zoetis.ca/sitemap.xml")
-				.then((res) => {
-					obj["/www.zoetis.ca"] = res.data;
-					console.log(obj);
-					callback(null, {
-						statusCode: 200,
-						body: obj,
-						headers: {
-							"Content-Type": "application/json",
-						},
-					});
-				})
-				.catch((error) => {
-					callback("www");
-				});
+			callback(null, {
+				statusCode: 200,
+				body: response.data,
+				headers: {
+					"Content-Type": "application/xml",
+				},
+			});
 		})
 		.catch((error) => {
-			callback("www2");
+			callback(error);
 		});
 };
