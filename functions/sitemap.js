d407889687fa37c4fd8df954297e68bb6ededc92
liamkopke <liamkopke@gmail.com>
2023-03-29 14:15:39 -0400

    logs

--- functions/sitemap.js
+++ functions/sitemap.js
@@ -6,10 +6,12 @@ exports.handler = async function (event, condition, callback) {
 		.get("https://www2.zoetis.ca/sitemap.xml")
 		.then((response) => {
 			obj["/www2.zoetis.ca"] = response.data;
+			console.log(obj);
 			axios
 				.get("https://www.zoetis.ca/sitemap.xml")
 				.then((res) => {
 					obj["/www.zoetis.ca"] = res.data;
+					console.log(obj);
 					callback(null, {
 						statusCode: 200,
 						body: obj,
