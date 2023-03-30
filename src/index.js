fc5bcd524c94ba20895834b6ba6b1d80596f767a
liamkopke <liamkopke@gmail.com>
2023-03-30 13:25:17 -0400

    new buffer

--- src/index.js
+++ src/index.js
@@ -24418,7 +24418,7 @@ fetch("/.netlify/functions/sitemap")
 		const netlifyJSON = { "/www2.zoetis.ca": {}, "/www.zoetis.ca": {} };
 		for (const site in passedJson) {
 			const xml = parser.parseFromString(
-				Buffer.from(passedJson[site], "base64").toString(),
+				new Buffer.from(passedJson[site], "base64").toString(),
 				"application/xml"
 			);
 			const urls = xml.getElementsByTagName("url");
